af3fa8c15f518b412d15ac36be6b363c4f270a25
Henrik Holmboe <henrik@dynamist.se>
2019-11-26 01:48:47 +0000

    The Git repo URL has nothing to do with it

--- scripts/install-podman.sh
+++ scripts/install-podman.sh
@@ -7,5 +7,3 @@ sudo apt -y install podman
 sudo mkdir -p /etc/containers
 sudo curl -s https://raw.githubusercontent.com/projectatomic/registries/master/registries.fedora -o /etc/containers/registries.conf
 sudo curl -s https://raw.githubusercontent.com/containers/skopeo/master/default-policy.json -o /etc/containers/policy.json
-
-https://github.com/dynamist/ansible-role-cloud9-bootstrap.git
