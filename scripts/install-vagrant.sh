af52426252b564c3bbe773a4b5d45ceef022aff5
Henrik Holmboe <henrik@dynamist.se>
2019-11-26 00:09:22 +0000

    Install vagrant-vbguest plugin

--- scripts/install-vagrant.sh
+++ scripts/install-vagrant.sh
@@ -3,3 +3,5 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key AD319E0F7CFFA38B4D9F6E55CE3F3DE92099F7A4
 sudo add-apt-repository "deb https://vagrant-deb.linestarve.com/ any main"
 sudo apt install -y vagrant
+
+vagrant plugin install vagrant-vbguest
